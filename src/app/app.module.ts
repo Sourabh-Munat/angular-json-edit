bc7ba93a072045a59be22bdd429e89268a13572a
Sourabh-Munat <sohrabjain23@gmail.com>
2020-04-16 13:23:55 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { FormsModule } from "@angular/forms";
 import { NgJsonEditorModule } from "ang-jsoneditor";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import {
