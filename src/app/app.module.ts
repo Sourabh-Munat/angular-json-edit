4e0fbf68655457a22ad8b795bb91278f702fff30
Sourabh-Munat <sohrabjain23@gmail.com>
2020-04-16 14:04:51 +0530

    Module import updates.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,33 +3,13 @@ import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgJsonEditorModule } from "ang-jsoneditor";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import {
-  MatInputModule,
-  MatProgressBarModule,
-  MatCardModule,
-  MatButtonModule,
-  MatTooltipModule,
-  MatChipsModule,
-  MatIconModule,
-  MatAutocompleteModule
+import { MatInputModule, MatProgressBarModule, MatCardModule MatButtonModule, MatTooltipModule, MatChipsModule, MatIconModule,MatAutocompleteModule
 } from "@angular/material";
 import { AppComponent } from "./app.component";
 import { EditJsonComponent } from "./edit-json.component";
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    FormsModule,
-    BrowserAnimationsModule,
-    NgJsonEditorModule,
-    MatInputModule,
-    MatProgressBarModule,
-    MatCardModule,
-    MatButtonModule,
-    MatTooltipModule,
-    MatChipsModule,
-    MatIconModule,
-    MatAutocompleteModule
+  imports: [ BrowserModule,FormsModule,BrowserAnimationsModule,NgJsonEditorModule,MatInputModule,MatProgressBarModule,MatCardModule,MatButtonModule,MatTooltipModule,MatChipsModule,MatIconModule,MatAutocompleteModule
   ],
   declarations: [AppComponent, EditJsonComponent],
   bootstrap: [AppComponent]
