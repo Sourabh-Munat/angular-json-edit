9f89ebc0c511894c9f12771abf26476e6233c45f
Sourabh-Munat <sohrabjain23@gmail.com>
2020-04-16 16:33:07 +0530

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgJsonEditorModule } from "ang-jsoneditor";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { MatInputModule, MatProgressBarModule, MatCardModule, MatButtonModule, MatTooltipModule, MatChipsModule, MatIconModule,MatAutocompleteModule } from "@angular/material";
+import { MatInputModule, MatProgressBarModule, MatCardModule, MatButtonModule, MatChipsModule, MatAutocompleteModule } from "@angular/material";
 import { AppComponent } from "./app.component";
 import { EditJsonComponent } from "./edit-json.component";
 
 @NgModule({
-  imports: [ BrowserModule,FormsModule,BrowserAnimationsModule,NgJsonEditorModule,MatInputModule,MatProgressBarModule,MatCardModule,MatButtonModule,MatTooltipModule,MatChipsModule,MatIconModule,MatAutocompleteModule
+  imports: [ BrowserModule,FormsModule,BrowserAnimationsModule,NgJsonEditorModule,MatInputModule,MatProgressBarModule,MatCardModule,MatButtonModule,MatChipsModule,MatAutocompleteModule
   ],
   declarations: [AppComponent, EditJsonComponent],
   bootstrap: [AppComponent]
