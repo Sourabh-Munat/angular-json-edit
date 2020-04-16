3dcbf7f239539e442625a4562db8444143b1b244
Sourabh-Munat <sohrabjain23@gmail.com>
2020-04-16 14:12:39 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,7 @@ import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgJsonEditorModule } from "ang-jsoneditor";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { MatInputModule, MatProgressBarModule, MatCardModule MatButtonModule, MatTooltipModule, MatChipsModule, MatIconModule,MatAutocompleteModule
-} from "@angular/material";
+import { MatInputModule, MatProgressBarModule, MatCardModule, MatButtonModule, MatTooltipModule, MatChipsModule, MatIconModule,MatAutocompleteModule } from "@angular/material";
 import { AppComponent } from "./app.component";
 import { EditJsonComponent } from "./edit-json.component";
 
